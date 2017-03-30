b41f87311d440ee0be7c434cfc60d9146b73a767
Ariel Gamino <gamino@yahoo.com>
2017-03-29 20:15:35 -0500

    Added exception to prevent sites not parsable by PhantomJS from stopping the program

--- src/step_1_capture_Json_responses.py
+++ src/step_1_capture_Json_responses.py
@@ -5,7 +5,8 @@ from util.phantomjs import netsniff
 #Loop throuh seed
 seeds_path  = "../seeds/"
 output_path = "../output/json/"
-seed_filename = "hospital_websites_urls_seed_3_v1.0.txt"
+# seed_filename = "hospital_websites_urls_seed_3_v1.0.txt"
+seed_filename = "hospital_websites_test.txt"
 
 """
    Read seename file and loop through all urls in it.
@@ -14,11 +15,15 @@ seed_filename = "hospital_websites_urls_seed_3_v1.0.txt"
 
 with open(seeds_path+seed_filename) as input_file:
     for url in input_file:
+        url = url.rstrip()
         print(url)
-        json = netsniff(url)
-        #create file name based on url
-        file_name = url.replace(".com/", ".com ").replace(".org/", ".org ").replace("https://", "").replace("http://","").replace("/", "").replace("?", "").replace("=", "").replace("&", "")
-        #write to file
-        file = open(output_path + file_name + '.json', 'w')
-        file.write(json)
-        file.close()
+        try:
+            json = netsniff(url)
+            #create file name based on url
+            file_name = url.replace(".com/", ".com ").replace(".org/", ".org ").replace("https://", "").replace("http://","").replace("/", "").replace("?", "").replace("=", "").replace("&", "").strip()
+            #write to file
+            file = open(output_path + file_name + '.json', 'w')
+            file.write(json)
+            file.close()
+        except:
+            print("There was an error with "+url)
